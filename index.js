d7e9b36e1b254532ea733debdf28c2afc517b1f4
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-20 16:32:49 -0400

    completed assignment

--- index.js
+++ index.js
@@ -24,6 +24,39 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const[moo,neigh,baa,oink,cluck] = farmAnimals.split(' ');
+console.log(moo,neigh,baa,oink,cluck);
+
+const[bessie, ,dolly,babe,little]= farmAnimals.split(' ')
+console.log(bessie,dolly,babe,little);
+
+const[blackAndWhite, ,black,pink, ,]= farmAnimals.split(' ');
+console.log(blackAndWhite,black,pink);
+
+const[red,orange,yellow,green,blue,indigo,violet] = colors;
+console.log(red,orange,yellow,green,blue,indigo,violet);
+
+const[r,o,y,g,b, ,v] = colors;
+console.log(r,o,y,g,b,v);
+
+const[, , , , ,indg, ,] = colors;
+console.log(indg);
+
+const{muppetName,color,song,job,partner}= muppet;
+muppetName;
+color;
+song;
+job;
+partner;
+
+const{song2,song4} = nestedMuppet.album.theMuppetMovie;
+song2;
+song4;
+
+const{nestedJob,nestedPartner}= nestedMuppet;
+console.log(nestedJob,nestedPartner);
+    
+
 
 // Strings
 
